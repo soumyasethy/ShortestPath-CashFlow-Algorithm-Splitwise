c8b11dab391e9f8d5eebd797ba294480c21e1f4b
Soumya Ranjan Sethy <sethy.soumyaranjan@gmail.com>
2017-07-24 20:09:16 +0530

    Added Minimum Shortest Money Flow Algorithm.

--- src/FindPath.java
+++ src/FindPath.java
@@ -1,5 +1,81 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+
 /**
- * Created by soumyaranjansethy on 24/07/17.
+ * Created by Soumya Ranjan Sethy on 24/07/17.
  */
 public class FindPath {
+
+    static HashMap hm;
+    static List printBill;
+
+    public static void main(String[] args) {
+        hm = new HashMap();
+        hm.put("A", -5.0);
+        hm.put("B", 25.0);
+        hm.put("C", -20.0);
+        hm.put("D", 25.0);
+        hm.put("E", -20.0);
+        hm.put("F", -5.0);
+        findPath(hm);
+
+        System.out.println(printBill.toString());
+
+    }
+
+
+    public static void findPath(HashMap details) {
+        printBill = new ArrayList();
+        Double Max_Value = (Double) Collections.max(details.values());
+        Double Min_Value = (Double) Collections.min(details.values());
+        if (Max_Value != Min_Value) {
+            String Max_Key = getKeyFromValue(details, Max_Value).toString();
+            String Min_Key = getKeyFromValue(details, Min_Value).toString();
+            Double result = Max_Value + Min_Value;
+            result = round(result, 1);
+            if ((result >= 0.0)) {
+                printBill.add(Min_Key + " needs to pay " + Max_Key + ":" + round(Math.abs(Min_Value), 2));
+                System.out.println(Min_Key + " needs to pay " + Max_Key + ":" + round(Math.abs(Min_Value), 2));
+                details.remove(Max_Key);
+                details.remove(Min_Key);
+                details.put(Max_Key, result);
+                details.put(Min_Key, 0.0);
+            } else {
+                printBill.add(Min_Key + " needs to pay " + Max_Key + ":" + round(Math.abs(Max_Value), 2));
+                System.out.println(Min_Key + " needs to pay " + Max_Key + ":" + round(Math.abs(Max_Value), 2));
+
+
+                details.remove(Max_Key);
+                details.remove(Min_Key);
+                details.put(Max_Key, 0.0);
+                details.put(Min_Key, result);
+            }
+            findPath(details);
+        }
+
+    }
+
+    public static Object getKeyFromValue(HashMap hm, Double value) {
+        for (Object o : hm.keySet()) {
+            if (hm.get(o).equals(value)) {
+                return o;
+            }
+        }
+        return null;
+    }
+
+    public static double round(double value, int places) {
+        if (places < 0)
+            throw new IllegalArgumentException();
+
+        BigDecimal bd = new BigDecimal(value);
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+
 }
